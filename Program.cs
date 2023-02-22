c811f477acdb81e3555cd4a9fe072bef3f233b84
Lust1 <leva95reva@yahoo.com>
2023-02-22 18:41:21 +0300

    Add main solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string[] array = new string[]{"hello", "2", "world", ":-)"};
+int resultArrayLength = 0;
+int j = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+        resultArrayLength++;
+}
+
+string[] resultArray = new string[resultArrayLength];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        resultArray[j] = array[i];
+        j++;
+    }
+        
+}
+
+Console.Write("[" + string.Join(",", resultArray) + "]");
